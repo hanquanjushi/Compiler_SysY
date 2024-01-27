c77a96adc9291213a3db80a1967fd48a389c0d35
wang <wrui0855@gmail.com>
2024-01-27 10:10:54 +0800

    add syntax tree func

--- test_use.sh
+++ test_use.sh
@@ -8,9 +8,12 @@ cd ..
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 #子目录
 target_dir="$script_dir/tests/func"
+
 #新文件
 output_dir="$script_dir/outputOFLAB"
+output_dir1="$script_dir/outputOFsyntaxtree"
 mkdir -p "$output_dir"
+mkdir -p "$output_dir1"
 # 输出文件名
 find "$target_dir" -type f -name "*.sy" -exec basename {} \; | while read -r filename; do
    # 去掉.sy
@@ -18,5 +21,7 @@ find "$target_dir" -type f -name "*.sy" -exec basename {} \; | while read -r fil
 
     #build
     ./build/lexer "$target_dir/$filename" > "$output_dir/$base_name.txt"
+    ./build/parser "$target_dir/$filename" > "$output_dir1/$base_name.txt"
+
 done
 
